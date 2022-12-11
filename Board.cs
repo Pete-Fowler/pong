91861fcd4d8e08ebe1f1561ff78392a0fdecc467
Pete Fowler <fowler.pete@gmail.com>
2022-12-11 10:53:09 -0700

    Complete draw board

--- Board.cs
+++ Board.cs
@@ -1,11 +1,16 @@
 public static class Board
 {
-    private const int WIDTH = 75;
-    private const int HEIGHT = 40;
-    private static string line = string.Join("", Enumerable.Repeat('=', WIDTH));
+    private const int WIDTH = 125;
+    private const int HEIGHT = 30;
+    private static string line = new String(char.Parse("#"), WIDTH);
 
     public static void Draw()
     {
+        Console.WriteLine(line);
+        for (int i = 0; i < HEIGHT; i++)
+        {
+            Console.WriteLine("#" + new String(char.Parse(" "), WIDTH - 2) + "#");
+        }
         Console.WriteLine(line);
     }
 }
